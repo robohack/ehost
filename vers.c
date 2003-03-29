c2a58f5edc6a776acf5a1c79ab3cd48e1bee1c09
Greg A. Woods <woods@planix.ca>
2003-03-29 02:53:04 +0000

    - just put $Name in the version string.

--- vers.c
+++ vers.c
@@ -2,13 +2,13 @@
  *	vers.c -- global version identifier
  */
 
-#ident "@(#)host:$Name:  $:$Id: vers.c,v 1.4 2003-03-28 22:19:57 -0800 woods Exp $"
+#ident "@(#)host:$Name:  $:$Id: vers.c,v 1.5 2003-03-29 02:53:04 -0800 woods Exp $"
 
 #if 0
 static char Version[] = "@(#)vers.c	e07@nikhef.nl (Eric Wassenaar) 991529";
 #endif
 
-char *version = "host version $Name:  $";
+char *version = "$Name:  $";
 
 #if defined(apollo)
 int h_errno = 0;		/* prevent shared library conflicts */
