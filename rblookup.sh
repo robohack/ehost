abae179440bb746982f7ae9347962f823b17a8ce
Greg A. Woods <woods@planix.ca>
2003-05-21 21:35:58 +0000

    - turn off MAPS by default - add -M to include MAPS - tweak docs. - wirehub -> easynet - remove xbl.selwerd.cx -- too radical. - add blitzed, REYNOLDS, spamhaus, SORBS

--- rblookup.sh
+++ rblookup.sh
@@ -1,21 +1,27 @@
 #! /bin/sh
 #
-#	@(#)rblookup            e07@nikhef.nl (Eric Wassenaar) 990127
+#ident "@(#)host:$Name:  $:$Id: rblookup.sh,v 1.5 2003-05-21 21:35:58 -0800 woods Exp $"
+#
+# rblookup - Lookup a dotted quad IP address, or hostname in one of many
+#		Reverse/Realtime DNS-based Lists
 #
 # Author:	E.Wassenaar, Nikhef-H
-# Version:	11-NOV-1997
-# Revision:	02-NOV-1998, Select MAPS (default) or alternatively ORBS
-# Revision:	07-NOV-1998, Anticipate hosts with multiple addresses
-# Revision:	27-JAN-1999, ORBS has moved to a new site
+#
+# Originally from:
+#	@(#)rblookup            e07@nikhef.nl (Eric Wassenaar) 990127
 #
 # Ongoing maintenance by Greg A. Woods <woods@planix.com>
 #
-#ident "@(#)host:$Name:  $:$Id: rblookup.sh,v 1.4 2003-05-17 00:58:43 -0800 woods Exp $"
+# Usage:
 #
-# Lookup a dotted quad IP address, or hostname in one of many
-# Reverse/Realtime Blackhole Lists
+#	rblookup [-Mdv] [-r rbl_list] hostname|IP#
 #
-#	Syntax:
+#		-M		- include the MAPS zones
+#		-r rbl_list	- use the specified RBLs
+#		-d 		- for debugging
+#		-v		- verbose output (show each RBL)
+#
+#	Operand Syntax:
 #		rblookup hostname ...
 #		rblookup dottedquad ...
 #		rblookup x.x.x.x.in-addr.arpa ...
@@ -24,7 +30,7 @@
 #		zero if the given host was found on the blacklist.
 #		nonzero if not, or if undetermined.
 #
-# The RBLs, or sometimes just DNS Black Lists (DNSBLs) are on-line,
+# The RBLs, or DNS Black/White Lists (DNS[BW]Ls) are on-line,
 # dynamically updated databases of spam hosts, maintained via the DNS.
 # The search key is the reversed dotted quad IP address of the given
 # host, within the zone of the RBL, for example "rbl.mail-abuse.com".
@@ -35,8 +41,9 @@
 # query for a TXT resource record.
 #
 # This script is just an example of a quick and dirty wrapper for the
-# ``host'' utility.  The technique can relatively easy be integrated
-# into MTA programs like sendmail.
+# ``host'' utility.  The technique it uses to check addresses can also
+# be relatively easily integrated into MTA programs like Smail, Exim,
+# Postrix, Sendmail, and so on.
 #
 # TODO:
 #
@@ -63,7 +70,7 @@ PATH=${BINDIR}:/bin:/usr/bin ; export PATH
 
 argv0=$(basename $0)
 
-options="[-dv] [-r rbl_list]"
+options="[-Mdv] [-r rbl_list]"
 usage="Usage: $argv0 $options hostname|IP# ..."
 reversed=""
 
@@ -76,18 +83,29 @@ reversed=""
 #
 #	http://www.declude.com/junkmail/support/ip4r.htm
 #
+#	http://moensted.dk/spam/	# less info, but also comprehensive
+#
 #	http://relays.osirusoft.com/cgi-bin/rbcheck.cgi	# has a test interface
 #
-#	http://openrbl.org/stats.htm	# very little info.
+#	http://openrbl.org/zones/	# incomplete, very little info, but has
+#					# an on-line test interface.
 #
-#	http://www.rbls.org/	# complete, but just a list and a test
+#	http://www.rbls.org/	# complete, but just a list and an on-line test
 #				# interface, no commentary.
 #
 #	http://www.iki.fi/era/rbl/rbl.html	# very dated 2001/12/02
 #
-# some published stats by one user of many DNS Black Lists.
+# some published stats by some users of DNS Black Lists.
 #
 #	http://www.sdsc.edu/~jeff/spam/Blacklists_Compared.html
+#	http://mail.vene.ws/cgi-bin/stats.pl?blacklist
+#	http://abuse.easynet.nl/spamstats.html
+#	http://basic.wirehub.nl/reports/
+#
+# more stats of some kind:
+#
+#	http://openrbl.org/stats.htm
+#
 
 # MAPS RBL: Mail Abuse Protection System Realtime Blackhole List
 # <URL:http://mail-abuse.org/rbl/>
@@ -96,8 +114,8 @@ reversed=""
 # blacklists of spam-originating systems.  Originally started by Paul
 # Vixie, this is the original "RBL" system.  Now a pay-for service.
 #
-MAPS_ROOT="blackholes.mail-abuse.org"
-ALL_RBLS="${MAPS_ROOT} ${ALL_RBLS}"
+MAPS_BLACKHOLES_ROOT="blackholes.mail-abuse.org"
+#ALL_RBLS="${MAPS_ROOT} ${ALL_RBLS}"
 
 # MAPS DUL: Mail Abuse Protection System Dial-up User List
 # <URL:http://mail-abuse.org/dul/>
@@ -109,8 +127,8 @@ ALL_RBLS="${MAPS_ROOT} ${ALL_RBLS}"
 # Al Iverson, the then-current DUL maintainer, to host it on their
 # systems.  Now a pay-for service.
 # 
-DUL_ROOT="dialups.mail-abuse.org"
-ALL_RBLS="${DUL_ROOT} ${ALL_RBLS}"
+MAPS_DUL_ROOT="dialups.mail-abuse.org"
+#ALL_RBLS="${DUL_ROOT} ${ALL_RBLS}"
 
 # MAPS RSS: MAPS Relay Spam Stopper
 # <URL:http://mail-abuse.org/rss/>
@@ -121,8 +139,8 @@ ALL_RBLS="${DUL_ROOT} ${ALL_RBLS}"
 # relays (and thus miss a vast number of the actively exploited
 # systems out there).  Now a pay-for service.
 #
-RSS_ROOT="relays.mail-abuse.org"
-ALL_RBLS="${RSS_ROOT} ${ALL_RBLS}"
+MAPS_RSS_ROOT="relays.mail-abuse.org"
+#ALL_RBLS="${RSS_ROOT} ${ALL_RBLS}"
 
 # ORBL: Open Relay Black List
 # <URL:http://www.orbl.org/>
@@ -308,6 +326,10 @@ ALL_RBLS="${MONKEYSPROXIES_ROOT} ${ALL_RBLS}"
 #
 # 127.0.0.2	lists open relays and known spam sources
 # 127.0.0.3	lists dial-up addresses
+# 127.0.0.4	lists spam sources
+# 127.0.0.5	lists multi-stage open relays
+# 127.0.0.8	lists servers with insecure formmail scripts
+# 127.0.0.9	lists open proxy sources
 #
 NJABL_ROOT="dnsbl.njabl.org"
 ALL_RBLS="${NJABL_ROOT} ${ALL_RBLS}"
@@ -338,12 +360,6 @@ ALL_RBLS="${DEVNULL_ROOT} ${ALL_RBLS}"
 #VISI_ROOT="relays.visi.com"
 #ALL_RBLS="${VISI_ROOT} ${ALL_RBLS}"
 
-# xbl.selwerd.cx spam List
-# <URL:http://selwerd.cx/xbl/>
-#
-XBL_ROOT="xbl.selwerd.cx"
-ALL_RBLS="${XBL_ROOT} ${ALL_RBLS}"
-
 # spambag.org spam List
 # <URL:http://www.spambag.org/>
 #
@@ -368,24 +384,74 @@ ALL_RBLS="${SPAMHAUS_ROOT} ${ALL_RBLS}"
 #SUMMIT_ROOT="blackholes.s2mbit.com"
 #ALL_RBLS="${SUMMIT_ROOT} ${ALL_RBLS}"
 
-# WireHub.nl lists
-# <URL:http://basic.wirehub.nl/spamstats.html>
+# Easynet.nl (formerly WireHub.nl) lists
+# <URL:http://abuse.easynet.nl/blackholes.html>
+# AUP at <URL:http://www.nl.easynet.net/pub/av/en/>
 #
-#     * Wirehub DynaBlocker (dynamic IP ranges; a lot of
+#     * Easynet DynaBlocker (dynamic IP ranges; a lot of
 #       spam comes straight from dial-up users)
 #
-#	<URL:http://basic.wirehub.nl/dynablocker.html>
+#	<URL:http://dynablock.easynet.nl/>
 #
-WIREHUB_DYNABLOCK_ROOT="dynablock.wirehub.net"
-ALL_RBLS="${WIREHUB_DYNABLOCK_ROOT} ${ALL_RBLS}"
+EASYNET_DYNABLOCK_ROOT="dynablock.easynet.nl"
+ALL_RBLS="${EASYNET_DYNABLOCK_ROOT} ${ALL_RBLS}"
 #
-#     * Wirehub DNSBL (IPs of persistent spammers, open
+#     * Easynet DNSBL (IPs of persistent spammers, open
 #       relay scanners & abusers, spamvertized websites)
 #
-#	<URL:http://basic.wirehub.nl/blackholes.html>
+#	<URL:http://blackholes.easynet.nl/>
 #
-WIREHUB_BLACKHOLES_ROOT="blackholes.wirehub.net"
-ALL_RBLS="${WIREHUB_BLACKHOLES_ROOT} ${ALL_RBLS}"
+EASYNET_BLACKHOLES_ROOT="blackholes.easynet.nl"
+ALL_RBLS="${EASYNET_BLACKHOLES_ROOT} ${ALL_RBLS}"
+#
+#     * Easynet Proxies (IPs of open proxies)
+#
+#	<URL:http://proxies.blackholes.easynet.nl/>
+#
+EASYNET_PROXIES_ROOT="proxies.blackholes.easynet.nl"
+ALL_RBLS="${EASYNET_PROXIES_ROOT} ${ALL_RBLS}"
+
+# Blitzed Open Proxy Monitor List
+# <URL:http://opm.blitzed.org/info>
+#
+#   WinGate      127.1.0.1
+#   SOCKS        127.1.0.2
+#   HTTP CONNECT 127.1.0.4
+#   Router       127.1.0.8
+#   HTTP POST    127.1.0.16
+#
+BLITZED_ALL_ROOT="opm.blitzed.org"
+ALL_RBLS="${BLITZED_ALL_ROOT} ${ALL_RBLS}"
+
+# reynolds boycott list
+# <URL:http://bl.reynolds.net.au/>
+#
+# ``comprises everything of "type 1", which is everything''
+#
+REYNOLDS_T1_BL_ROOT="t1.bl.reynolds.net.au"
+ALL_RBLS="${REYNOLDS_T1_BL_ROOT} ${ALL_RBLS}"
+
+# Spamhaus Block List
+# <URL:http://www.spamhaus.org/sbl/index.lasso>
+#
+# verified spam sources
+#
+SPAMHAUS_BL_ROOT="sbl.spamhaus.org"
+ALL_RBLS="${SPAMHAUS_BL_ROOT} ${ALL_RBLS}"
+
+# SORBS: Spam and Open Relay Blocking System
+# <URL:http://www.dnsbl.sorbs.net/>
+#
+# 127.0.0.2	- open HTTP proxies
+# 127.0.0.3	- open SOCKS proxies
+# 127.0.0.4	- misc proxies
+# 127.0.0.5	- open relays
+# 127.0.0.6	- sent spam to sorbs.net
+# 127.0.0.7	- vulnerable web servers
+# 127.0.0.9	- hijacked networks
+#
+SORBS_ROOT="dnsbl.sorbs.net"
+ALL_RBLS="${SORBS_ROOT} ${ALL_RBLS}"
 
 # ----------------------------------------------------------------------
 # Exit codes from <sysexits.h>, just in case we are called from a mailer
@@ -415,6 +481,9 @@ rbllist=""
 
 for i in ${1+$@}; do
 	case "$i" in
+	-M)
+		ALL_RBLS="${MAPS_BLACKHOLES_ROOT} ${MAPS_DUL_ROOT} ${MAPS_RSS_ROOT} ${ALL_RBLS}"
+		;;
 	-d)
 		show_or_exec=echo
 		;;
