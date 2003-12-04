7150660a7a4619cddcdad409c60a2c55b0ae00a2
Greg A. Woods <woods@planix.ca>
2003-12-04 03:41:18 +0000

    - convert to using getopts. - use '-vP' to find the true parent zone. - add '-c' to allow query class to be specified. - add '-V' as an alias for '-c ch -t txt'.

--- nscheck.sh
+++ nscheck.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-#ident "@(#)host:$Name:  $:$Id: nscheck.sh,v 1.3 2003-12-01 19:05:12 -0800 woods Exp $"
+#ident "@(#)host:$Name:  $:$Id: nscheck.sh,v 1.4 2003-12-04 03:41:18 -0800 woods Exp $"
 #
 #	nscheck - check NS RRs at every parent NS
 #
@@ -9,6 +9,19 @@
 # checking for NS records by default
 #
 # For now the comparison must be done visually.
+#
+# Using '-a' should also show any glue records the parent nameservers
+# may have -- however modern BIND-9 will return delegations in the
+# AUTHORITY section and thus '-a' will fail and no glue records will
+# ever be returned.
+#
+# The following is fun:
+#
+#	nscheck -V -P . version.bind
+#
+# it's the equivalent of:
+#
+#	nscheck -t txt -c ch -P . version.bind
 
 # Exit codes from <sysexits.h>
 #
@@ -19,57 +32,52 @@ EX_UNAVAILABLE=69
 # default setup
 #
 flags="-r"
-type="ns"
+type="NS"
+pzone=""
+class=""
 rc=""
 
-for i in ${1+$@}; do
-	case "$i" in
-	-d|-u|-v|-Z|-r)
-		flags="$i $flags"
+argv0=$(basename $0)
+
+while getopts a:c:dP:rt:uvVZ OPT; do
+	case "$OPT" in
+	d|u|v|Z|r)
+		flags="-$OPT $flags"
 		;;
-	-a)
-		type="any"
+	a)
+		type="ANY"
 		;;
-	-t)
-		type="$2"
-		shift
+	t)
+		type="$OPTARG"
 		;;
-	-P)
-		parent="$2"
-		shift
+	c)
+		class="-c $OPTARG"
 		;;
-	-*)
-		echo "$argv0: Unknown option $i" 1>&2
+	P)
+		pzone="$OPTARG"
+		;;
+	V)
+		type="TXT"
+		class="-c CH"
+		;;
+	\?)
 		exit $EX_USAGE
 		;;
-	*)
-		break
-		;;
 	esac
-	shift
 done
+shift $(($OPTIND - 1))
 
-for domain
-do
-	# XXX the parent domain needs to be searched for since the
-	# immediate parent zone may not be a valid zone, e.g. as in
-	# "weird.toronto.on.ca" where the true parent zone is "ca"
-	#
-	# For now the work-around is to use '-P true.parent.zone'
-	#
-	# The proper fix will be to learn the value for $nslist by
-	# using the new '-P' option:  "host -P -t ns $domain"
-	#
-	if [ -z "$parent" ] ; then
-		parent=$(echo $domain | sed 's/[^\.]*.//')
+for domain in $@; do
+	if [ -z "$pzone" ]; then
+		pzone=$(host -v -P $domain 2> /dev/null | fgrep 'parent zone:' | tail -1 | awk '{sub(/\)/, "", $NF); print $NF}')
 	fi
-	case "$parent" in
+	case "$pzone" in
 	*.*)
 		;;
-	*)
-		parent="${parent}."
+	*)	
+		pzone=${pzone}.
 	esac
-	nslist=$(host -t ns $parent | awk '{print $NF}')
+	nslist=$(host --canonskip -t NS $pzone | awk '{print $NF}')
 	for ns in $nslist ; do
 		echo ""
 		case "$flags" in
@@ -78,7 +86,7 @@ do
 			;;
 		esac
 		echo "====> Checking $ns for $type records for $domain...."
-		host $flags -t $type $domain $ns
+		host $flags $class -t $type $domain $ns
 		rc=${rc:-$?}
 	done
 done
