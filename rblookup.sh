8f1910eb97ac7660656f41b6b15790ad8b57c4d3
Greg A. Woods <woods@robohack.ca>
2023-09-19 16:33:25 -0700

    rblookup.sh: update the default lists of blacklists

--- rblookup.sh
+++ rblookup.sh
@@ -40,6 +40,8 @@
 # being queried.  Additional info may often be found via an extra
 # query for a TXT resource record.
 #
+# See RFC-5782 <URL:https://www.rfc-editor.org/rfc/rfc5782>
+#
 # This script is just an example of a quick and dirty wrapper for the
 # ``host'' utility.  The technique it uses to check addresses can also
 # be relatively easily integrated into MTA programs like Smail, Exim,
@@ -47,7 +49,7 @@
 #
 # TODO:
 #
-#	- support name-based DNS Black Lists (RHSBL) (eg. rfc-ignorant.org)
+#	- support name-based DNS Black Lists (RHSBL)
 #
 #	- move all the DNSBLs to a configuration file, perhaps tagging
 #	  each as either default or optional, selecting all with a
@@ -68,242 +70,55 @@ reversed=""
 # ----------------------------------------------------------------------
 #
 # A semi-canonical list of RBLs and associated commentary can be found
-# at both of the following sites:
+# at the following sites:
 #
-#	https://whatismyipaddress.com/blacklist-check
+#	https://en.wikipedia.org/wiki/Comparison_of_DNS_blacklists
 #
-#	http://www.rbls.org/	# complete, but just an on-line test interface,
+#	https://multirbl.valli.org/lookup/		# needs JavaScript
+#
+#	https://whatismyipaddress.com/blacklist-check	# needs JavaScript
+#
+#	https://www.rbls.org/	# complete, but just an on-line test interface,
 #				# no commentary.
 #
 #	https://www.rtsak.com/dns-lookup/	# does many things, including
 #						# the rbls.org DNSBL lookups
 #
-#	http://www.DNSstuff.com/	# much more than just blacklist testing!
-#
-# some published stats by some users of DNS Black Lists.
-#
-#	http://www.sdsc.edu/~jeff/spam/Blacklists_Compared.html
-#	http://mail.vene.ws/cgi-bin/stats.pl?blacklist
-#	http://abuse.easynet.nl/spamstats.html
-#	http://www.dnsbl.net.au/stats/
-#
 
-##smtp_rbl_domains = "\
-##        :zen.spamhaus.org;127.0.0.2, 127.0.0.4, 127.0.0.5, 127.0.0.6, 127.0.0.7,127.0.0.8, 127.0.0.10, 127.0.0.11\
-##        :psbl.surriel.com;127/8\
-##        :cbl.abuseat.org;127/8\
-##        :dnsbl.sorbs.net;127.0.0.2, 127.0.0.3, 127.0.0.4, 127.0.0.5, 127.0.0.6,127.0.0.7, 127.0.0.8, 127.0.0.9, 127.0.0.10, 127.0.0.11,127.0.0.12\
-##        :spamsources.fabel.dk;127.0.0.2\
-##        :bl.spamcop.net;127.0.0.2\
-
-
-# The Abusive Hosts Blocking List
-# <URL:http://www.ahbl.org/>
+# The LashBack "unsubscribe" abuser list:
+# <URL:https://blacklist.lashback.com/>
 #
-# N.B.:  This zone now blacklists everything, and the TXT says:
+# no TXT RRs, not testable (i.e. not RFC-5782 compliant).
 #
-#	"List shut down.  See: http://www.ahbl.org/content/last-notice-wildcarding-services-jan-1st"
-#
-#AHBL_ALL_ROOT="dnsbl.ahbl.org"
-#ALL_RBLS="${AHBL_ALL_ROOT} ${ALL_RBLS}"
-
-# Blitzed Open Proxy Monitor List
-# <URL:http://opm.blitzed.org/info>
-#
-#   WinGate      127.1.0.1
-#   SOCKS        127.1.0.2
-#   HTTP CONNECT 127.1.0.4
-#   Router       127.1.0.8
-#   HTTP POST    127.1.0.16
-#
-BLITZED_ALL_ROOT="opm.blitzed.org"
-ALL_RBLS="${BLITZED_ALL_ROOT} ${ALL_RBLS}"
-
-# dev.null.dk Open Relay List
-# <URL:http://dev.null.dk/bl.shtml>
-#
-DEVNULL_ROOT="dev.null.dk"
-ALL_RBLS="${DEVNULL_ROOT} ${ALL_RBLS}"
-
-# DNSRBL - 
-# <URL:http://www.dnsrbl.com/>
-#
-# 127.0.0.2	verified open relay
-# 127.0.0.3	dialup spam source
-# 127.0.0.4	confirmed spam source
-# 127.0.0.5	smart host
-# 127.0.0.6	a spamware software developer or spamvertized site
-# 127.0.0.7	a list server that opts in without confirmation
-# 127.0.0.8	an insecure formmail.cgi script
-# 127.0.0.9	open proxy servers
-#
-# GONE?!?!?!?
-#
-#DNSRBL_ROOT="dun.dnsrbl.net spam.dnsrbl.net"
-#ALL_RBLS="${DNSRBL_ROOT} ${ALL_RBLS}"
+UNSUB_ROOT="ubl.unsubscore.com"
+ALL_RBLS="${ALL_RBLS} ${UNSUB_ROOT}"
 
 # DSBL - Distributed Sender Boycott List
 # <URL:http://www.dsbl.org/>
 #
-DSBL_ROOT="list.dsbl.org unconfirmed.dsbl.org multihop.dsbl.org"
-ALL_RBLS="${DSBL_ROOT} ${ALL_RBLS}"
+# Replaced by Passive Spam Block List
+# <URL:https://psbl.org/>
+#
+PSBL_ROOT="psbl.surriel.com"
+ALL_RBLS="${ALL_RBLS} ${PSBL_ROOT}"
 
 # ABUSEAT lists
 # The CBL - Composite Blocking List
-# <URL:http://www.abuseat.org/>
-# <URL:http://cbl.abuseat.org/>
+# <URL:https://www.abuseat.org/>
+# <URL:https://cbl.abuseat.org/>
 #
 # The CBL operates in an entirely automated way using very large
 # spamtraps and smart heuristics to detect real spam sources. There
 # is an automated no-questions-asked removals procedure too.
 #
-ABUSEAT_RSBL_ROOT="cbl.abuseat.org"
-ALL_RBLS="${ABUSEAT_RSBL_ROOT} ${ALL_RBLS}"
-
-# McFadden Associates Spam Blacklist
-# <URL:http://bl.csma.biz/>
-#
-# N.B.:  Dead and gone.
-#
-#CSMA_BL_ROOT="bl.csma.biz"
-#ALL_RBLS="${CSMA_BL_ROOT} ${ALL_RBLS}"
-#CSMA_SBL_ROOT="sbl.csma.biz"
-#ALL_RBLS="${CSMA_SBL_ROOT} ${ALL_RBLS}"
-
-# five-ten-sg.com blackholes
-# <URL:http://www.five-ten-sg.com/blackhole.php>
-#
-# N.B.:  Dead and gone.
-#
-# 127.0.0.2	Lists direct spam sources (by netblock for the misc.spam group).
-# 127.0.0.3	Lists spam sites before they get into DUL
-# 127.0.0.4	Lists bulk mailers that don't use confirmed opt-in
-# 127.0.0.5	Lists multi-stage open relays
-# 127.0.0.6	Lists single-stage open relays
-# 127.0.0.7	Lists IP ranges of companies that ignore spam complaints
-# 127.0.0.8	Lists servers running vulnerable web scripts that can send spam
-# 127.0.0.9	Lists servers with "other issues."
-#
-#FIVETENSG_ROOT="blackholes.five-ten-sg.com"
-#ALL_RBLS="${FIVETENSG_ROOT} ${ALL_RBLS}"
-
-# N.B.:  mail-abuse.org has been taken over by trendmicro.com!
-#
-# MAPS RBL: Mail Abuse Protection System Realtime Blackhole List
-# <URL:http://mail-abuse.org/rbl/>
-# 
-# This listing is human-operated, and is one of the most popular
-# blacklists of spam-originating systems.  Originally started by Paul
-# Vixie, this is the original "RBL" system.  Now a pay-for service.
-#
-#MAPS_BLACKHOLES_ROOT="blackholes.mail-abuse.org"
-#ALL_RBLS="${MAPS_ROOT} ${ALL_RBLS}"
-
-# MAPS DUL: Mail Abuse Protection System Dial-up User List
-# <URL:http://mail-abuse.org/dul/>
-# 
-# This list was started from a list compiled by "Afterburner" (the
-# head-honcho abuse guy at Erols Internet), and extended to be easily
-# added to over time.  This service was originally called the ORCA
-# DUL, and was operated at "dul.orca.bc.ca".  The MAPS project invited
-# Al Iverson, the then-current DUL maintainer, to host it on their
-# systems.  Now a pay-for service.
-# 
-#MAPS_DUL_ROOT="dialups.mail-abuse.org"
-#ALL_RBLS="${DUL_ROOT} ${ALL_RBLS}"
-
-# MAPS RSS: MAPS Relay Spam Stopper
-# <URL:http://mail-abuse.org/rss/>
-# 
-# This is a list of e-mail relays which have been verified to have
-# been used to send spam.  They supposedly have archives of the spam
-# they received through these relays.  They do not list multi-level
-# relays (and thus miss a vast number of the actively exploited
-# systems out there).  Now a pay-for service.
-#
-#MAPS_RSS_ROOT="relays.mail-abuse.org"
-#ALL_RBLS="${RSS_ROOT} ${ALL_RBLS}"
-
-# Not Just Another Black List
-# <URL:http://njabl.org/>
-# <URL:http://njabl.org/dynablock.html> (gone -- use pbl.spamhaus.org)
-#
-# N.B.:  Dead and gone.
-#
-# 127.0.0.2	lists open relays and known spam sources
-# 127.0.0.3	lists dial-up addresses
-# 127.0.0.4	lists spam sources
-# 127.0.0.5	lists multi-stage open relays
-# 127.0.0.8	lists servers with insecure formmail scripts
-# 127.0.0.9	lists open proxy sources
-#
-#NJABL_ROOT="dnsbl.njabl.org"
-#ALL_RBLS="${NJABL_ROOT} ${ALL_RBLS}"
-
-# ORDB - third son of ORBS
-# <URL:http://www.ordb.org/>
-#
-# ORDB is shutting down as of 2006/12/18
-#
-#ORDB_ROOT="relays.ordb.org "
-#ALL_RBLS="${ORDB_ROOT} ${ALL_RBLS}"
-
-# 
-# <URL:http://dnsbl.net.au/>
-#
-# (formerly the reynolds boycott list)
-#
-# Note this is strictly a pay-for subscription service and must not be
-# used for production without a subscription.
-#
-# ``comprises everything of "type 1", which is everything''
-#
-DNSBL_NET_AU_ROOT="t1.dnsbl.net.au"
-ALL_RBLS="${REYNOLDS_T1_BL_ROOT} ${ALL_RBLS}"
-
-# rfc-ignorant IP-based whois List
-# <URL:http://www.rfc-ignorant.org/policy-ipwhois.php>
-#
-# list of nets without proper WHOIS information
-#
-RFC_IGNORANT_IPWHOIS_ROOT="ipwhois.rfc-ignorant.org"
-ALL_RBLS="${RFC_IGNORANT_IPWHOIS_ROOT} ${ALL_RBLS}"
-
-# spambag.org spam List
-# <URL:http://www.spambag.org/>
-#
-# list of spam support service providers and such
-#
-# They also had a whitelist
-#
-# N.B.:  Dead and gone.
-#
-#SPAMBAG_ROOT="blacklist.spambag.org"
-#ALL_RBLS="${SPAMBAG_ROOT} ${ALL_RBLS}"
+ABUSEAT_ROOT="cbl.abuseat.org"
+ALL_RBLS="${ALL_RBLS} ${ABUSEAT_ROOT}"
 
 # SpamCop Blocking List
-# <URL:http://spamcop.net/bl.shtml>
+# <URL:https://spamcop.net/bl.shtml>
 #
 SPAMCOP_ROOT="bl.spamcop.net"
-ALL_RBLS="${SPAMCOP_ROOT} ${ALL_RBLS}"
-
-# SpamGuard by Howard Leadmon
-# <URL:http://www.leadmon.net/spamguard/>
-#
-# 127.0.0.2	Dial-Up IP Addresses
-# 127.0.0.3	Individual SPAM Sources
-# 127.0.0.4	Bulk mailers
-# 127.0.0.5	Single-Stage Open Relays
-# 127.0.0.6	Multi-Stage Open Relays
-# 127.0.0.7	SpamBlock Sites (have sent us direct SPAM)
-#
-# 127.0.0.1 will always return a negative (not found) result, and can
-# be used to verify no response. As such a 127.0.0.2 will return a
-# positive on the dialup section, and 127.0.0.3 will return a positive
-# on the spam section.  And so on....  (except for '7' 2002/03/21)
-#
-SPAMGUARD_ROOT="spamguard.leadmon.net"
-ALL_RBLS="${SPAMGUARD_ROOT} ${ALL_RBLS}"
+ALL_RBLS="${ALL_RBLS} ${SPAMCOP_ROOT}"
 
 # Spamhaus Block List
 # <URL:http://www.spamhaus.org/zen/>
@@ -311,17 +126,37 @@ ALL_RBLS="${SPAMGUARD_ROOT} ${ALL_RBLS}"
 # ZEN contains all of the SBL, XBL, and PBL
 #
 SPAMHAUS_ZEN_ROOT="zen.spamhaus.org"
-ALL_RBLS="${SPAMHAUS_ZEN_ROOT} ${ALL_RBLS}"
+ALL_RBLS="${ALL_RBLS} ${SPAMHAUS_ZEN_ROOT}"
 
-# spamsources.fabel.dk blackholes
-# <URL:http://www.fabel.dk/relay/>
+# 0Spam Project
+# <URL:https://www.0spam.org/>
 #
-# They only do the most basic relay test with plain envelope addresses.
+ZEROSPAM_ROOT="bl.0spam.org"
+ALL_RBLS="${ALL_RBLS} ${ZEROSPAM_ROOT}"
+
+# JustSpam
+# <URL:http://www.justspam.org/>
 #
-# 127.0.0.2	Lists direct spam sources.
+JUSTSPAM_ROOT="dnsbl.justspam.org"
+ALL_RBLS="${ALL_RBLS} ${JUSTSPAM_ROOT}"
+
+# SpamRats
+# <URL:https://www.spamrats.com/>
 #
-SPAMSOURCES_FABEL_ROOT="spamsources.fabel.dk"
-ALL_RBLS="${SPAMSOURCES_FABEL_ROOT} ${ALL_RBLS}"
+SPAMRATS_NOPTR_ROOT="noptr.spamrats.com"
+SPAMRATS_DYNA_ROOT="dyna.spamrats.com"
+SPAMRATS_SPAM_ROOT="spam.spamrats.com"
+SPAMRATS_AUTH_ROOT="auth.spamrats.com"
+SPAMRATS_ALL_ROOT="all.spamrats.com"
+ALL_RBLS="${ALL_RBLS} ${SPAMRATS_ALL_ROOT}"
+
+# The NordSpam Project
+# <URL:https://www.nordspam.com/>
+#
+# also have a domain blacklist (and a whitelist)
+#
+NORDSPAM_ROOT="bl.nordspam.com"
+ALL_RBLS="${ALL_RBLS} ${NORDSPAM_ROOT}"
 
 # SORBS: Spam and Open Relay Blocking System
 # <URL:http://www.dnsbl.sorbs.net/>
@@ -339,29 +174,116 @@ ALL_RBLS="${SPAMSOURCES_FABEL_ROOT} ${ALL_RBLS}"
 # 127.0.0.12	- domains that are never used for e-mail
 #
 SORBS_ROOT="dnsbl.sorbs.net"
-ALL_RBLS="${SORBS_ROOT} ${ALL_RBLS}"
+ALL_RBLS="${ALL_RBLS} ${SORBS_ROOT}"
 
-# VISI.com Relay Stop List
-# <URL:http://relays.visi.com/>
+# spamsources.fabel.dk blackholes
+# <URL:http://www.fabel.dk/relay/>
 #
-# Only list confirmed open relays that have sent spam, and
-# automatically de-list after 90 days.  Apparently they'll gladly
-# remove any listing on request too.
+# They only do the most basic relay test with plain envelope addresses.
 #
-VISI_ROOT="relays.visi.com"
-ALL_RBLS="${VISI_ROOT} ${ALL_RBLS}"
+# 127.0.0.2	Lists direct spam sources.
+#
+SPAMSOURCES_FABEL_ROOT="spamsources.fabel.dk"
+ALL_RBLS="${ALL_RBLS} ${SPAMSOURCES_FABEL_ROOT}"
+
+# SpamGuard by Howard Leadmon
+# <URL:http://www.leadmon.net/spamguard/>
+#
+# 127.0.0.2	Dial-Up IP Addresses
+# 127.0.0.3	Individual SPAM Sources
+# 127.0.0.4	Bulk mailers
+# 127.0.0.5	Single-Stage Open Relays
+# 127.0.0.6	Multi-Stage Open Relays
+# 127.0.0.7	SpamBlock Sites (have sent us direct SPAM)
+#
+# 127.0.0.1 will always return a negative (not found) result, and can
+# be used to verify no response. As such a 127.0.0.2 will return a
+# positive on the dialup section, and 127.0.0.3 will return a positive
+# on the spam section.  And so on....  (except for '7' 2002/03/21)
+#
+SPAMGUARD_ROOT="spamguard.leadmon.net"
+ALL_RBLS="${ALL_RBLS} ${SPAMGUARD_ROOT}"
 
 # ANTISPAM-UFRJ
 # <URL:http://www.aupads.org/>
 #
-# no TXT RRs, not testable.
+# no TXT RRs, not testable (i.e. not RFC-5782 compliant).
 #
-UFRJ_RSBL_ROOT="rsbl.aupads.org"
-ALL_RBLS="${UFRJ_RSBL_ROOT} ${ALL_RBLS}"
+# RSBL suspended for now.....
+#UFRJ_RSBL_ROOT="rsbl.aupads.org"
+#ALL_RBLS="${ALL_RBLS} ${UFRJ_RSBL_ROOT}"
+# DUINV discontinued!
+#DUINV_RSBL_ROOT="duinv.aupads.org"
+#ALL_RBLS="${ALL_RBLS} ${DUINV_RSBL_ROOT}"
 ORVEDB_RSBL_ROOT="orvedb.aupads.org"
-ALL_RBLS="${ORVEDB_RSBL_ROOT} ${ALL_RBLS}"
-DUINV_RSBL_ROOT="duinv.aupads.org"
-ALL_RBLS="${DUINV_RSBL_ROOT} ${ALL_RBLS}"
+ALL_RBLS="${ALL_RBLS} ${ORVEDB_RSBL_ROOT}"
+
+# Mailspike - Anubisnetworks reputation and Zero-Hour blocklists
+# <URL:https://www.mailspike.net/>
+#
+# "bl" is the combined reputation ("rep") and zero-hour ("z")
+#
+# They also have a "whitelist", wl.mailspike.net
+#
+MAILSPIKE_ROOT="bl.mailspike.net"
+ALL_RBLS="${ALL_RBLS} ${MAILSPIKE_ROOT}"
+
+# Good, Bad, Ugly database
+# <URL:http://gbudb.com/>
+#
+GBUDB_ROOT="truncate.gbudb.net"
+ALL_RBLS="${ALL_RBLS} ${GBUDB_ROOT}"
+
+# Weighted Private Block List
+# <URL:http://www.wpbl.info/>
+#
+WPBL_ROOT="db.wpbl.info"
+ALL_RBLS="${ALL_RBLS} ${WPBL_ROOT}"
+
+# Metunet Research Labs
+# <URL:>
+#
+METUNET_ROOT="rbl.metunet.com"
+ALL_RBLS="${ALL_RBLS} ${METUNET_ROOT}"
+
+# Blocklist.de
+# <URL:http://www.blocklist.de/en/api.html#dns>
+#
+BLOCKLIST_DE_ROOT="bl.blocklist.de"
+ALL_RBLS="${ALL_RBLS} ${BLOCKLIST_DE_ROOT}"
+
+# NiX Spam
+# <URL:https://www.nixspam.net/>
+#
+NIXSPAM_ROOT="ix.dnsbl.manitu.net"
+ALL_RBLS="${ALL_RBLS} ${NIXSPAM_ROOT}"
+
+# s5h.net
+# <URL:http://www.usenix.org.uk/content/rbl.html>
+#
+S5H_ROOT="all.s5h.net"
+ALL_RBLS="${ALL_RBLS} ${S5H_ROOT}"
+
+# BarracudaCentral
+# <URL:http://www.barracudacentral.org/rbl>
+#
+# Requires registration of recursive NS resolver source address(es)
+#
+# no TXT RRs, not testable (i.e. not RFC-5782 compliant).
+#
+BARRACUDA_ROOT="bl.barracudacentral.org"
+ALL_RBLS="${ALL_RBLS} ${BARRACUDA_ROOT}"
+
+# The Abusive Hosts Blocking List
+# <URL:https://www.ahbl.org/>
+#
+# N.B.:  Invite only!  The old zone now blacklists everything, and the TXT says:
+#
+#	"List shut down.  See: http://www.ahbl.org/content/last-notice-wildcarding-services-jan-1st"
+#
+#AHBL_ALL_ROOT="dnsbl.ahbl.org"
+#ALL_RBLS="${ALL_RBLS} ${AHBL_ALL_ROOT}"
+
 
 # ----------------------------------------------------------------------
 # Exit codes from <sysexits.h>, just in case we are called from a mailer
